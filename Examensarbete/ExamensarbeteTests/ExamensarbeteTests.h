b69e6fb1e2f5f39d9201e17ab85f951bd8c2fc7b
Johan Karlgrund <jkarlgrund@gmail.com>
2012-02-14 13:32:30 +0100

    commit

---
+++ Examensarbete/ExamensarbeteTests/ExamensarbeteTests.h
@@ -0,0 +1,13 @@
+//
+//  ExamensarbeteTests.h
+//  ExamensarbeteTests
+//
+//  Created by Johan Karlgrund on 2/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ExamensarbeteTests : SenTestCase
+
+@end
---
+++ Examensarbete/ExamensarbeteTests/ExamensarbeteTests.m
@@ -0,0 +1,32 @@
+//
+//  ExamensarbeteTests.m
+//  ExamensarbeteTests
+//
+//  Created by Johan Karlgrund on 2/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ExamensarbeteTests.h"
+
+@implementation ExamensarbeteTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ExamensarbeteTests");
+}
+
+@end
